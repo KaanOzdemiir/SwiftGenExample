733f1bf6c013f281b563b50b3b745a4f297da280
KaanOzdemir <kaan.ozdemir@tsoft.com.tr>
2021-10-10 15:15:46 +0300

    Initial Commit

---
+++ SwiftGenExample/SwiftGenExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenExampleApp.swift
+//  SwiftGenExample
+//
+//  Created by Kaan Ozdemir on 10.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
